5f30b7c38bbd4ac8160f392b1b2e1827e397be4f
Denis T <denistex.dev@gmail.com>
2024-05-24 15:00:43 +0200

    feat: add mappings and abbreviations

---
+++ lua/abbreviations.lua
@@ -0,0 +1,5 @@
+local cmd = vim.cmd
+
+cmd('iabbrev teh the')
+cmd('iabbrev tehn then')
+cmd('iabbrev calss class')
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,13 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jj", "<ESC>")
+
+map("i", "jk", "<ESC>")
+map("i", "<ESC>", "<NOP>")
+map("i", "<C-c>", "<NOP>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 map("n", "<leader>j", "<cmd>HopWord<CR>", { desc = "hop word" })
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "toggle nvimtree" })
+map("n", "<leader>a", "<cmd>AerialToggle<CR>", { desc = "toggle aerial" })
