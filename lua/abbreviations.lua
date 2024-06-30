a5ab1ba08e1d326c3f30b47974eaaf8f623b0dd3
Denis T <denistex.dev@gmail.com>
2024-06-30 14:29:06 +0200

    feat: add more abbreviations

--- lua/abbreviations.lua
+++ lua/abbreviations.lua
@@ -1,5 +1,9 @@
 local cmd = vim.cmd
 
+cmd('iabbrev DateTiem DateTime')
+cmd('iabbrev calss class')
+cmd('iabbrev stesp steps')
+cmd('iabbrev stirng string')
+cmd('iabbrev strign string')
 cmd('iabbrev teh the')
 cmd('iabbrev tehn then')
-cmd('iabbrev calss class')
