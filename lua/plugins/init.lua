00b3e92d4183b24be16e325c36765370bc4b50c7
Denis T <denis@eproval.com>
2025-02-09 21:22:32 +0100

    feat: add the hop plugin
    See also:
    https://github.com/smoka7/hop.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,6 +18,15 @@ return {
     },
   },
 
+  {
+    "smoka7/hop.nvim",
+    version = "*",
+    cmd = { "HopWord" },
+    opts = {
+      keys = "etovxqpdygfblzhckisuran"
+    },
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
