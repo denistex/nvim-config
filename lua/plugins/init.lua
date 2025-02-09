489370ae255837a3e0e4b75ca8bf9e536f5ac5b5
Denis T <denis@eproval.com>
2025-02-09 21:15:57 +0100

    feat: adjust telescope settings

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,19 @@ return {
     opts = require "configs.conform",
   },
 
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        winblend = 10,
+        layout_strategy = "vertical",
+        layout_config = {
+          preview_cutoff = 50,
+        },
+      },
+    },
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
